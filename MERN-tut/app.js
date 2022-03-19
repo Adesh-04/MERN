d3d1c7c2383e83e5724bf26d2aeb9c6eac820fc2
Adesh-04 <AdeshGadekar98@gmail.com>
2022-03-19 15:55:23 +0530

    more routes added

--- MERN-tut/app.js
+++ MERN-tut/app.js
@@ -1,8 +1,28 @@
+console.log('Command line working ' )
+
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send('Hello World from Express')
+    res.send('Hello World from Express');
 }) ;                                         // syntax: app.get( path, callback)
 
-console.log('Command line working Properly ' )
+app.get('/about', (req, res)=> {
+    res.send('Here you can see my information');
+}) ;                                         
+
+app.get('/contact', (req, res)=> {
+    res.send('This is my contact id');
+}) ; 
+
+app.get('/login', (req, res)=> {
+    res.send('Hello, welcome back');
+}) ;     
+
+app.get('/sigin', (req, res)=> {
+    res.send('Hello, good to see you');
+}) ;                                         
+
+app.listen(3000, ()=> {
+    console.log('SERVER is running at port 3000');
+}) ;                                        // syntax: app.listen( port, callback)
